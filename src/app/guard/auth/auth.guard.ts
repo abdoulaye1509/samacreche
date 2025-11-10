5a8a5e36ed282346be4c2c54ec80ba6fccc2152e
Abdoulaye Wélé <weleabdoulaye15@gmail.com>
2025-11-10 15:54:21 +0000

    xvc

--- src/app/guard/auth/auth.guard.ts
+++ src/app/guard/auth/auth.guard.ts
@@ -2,24 +2,23 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { ApiService } from '../../service/api/api.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private api:ApiService,private routage:Router) { }
-  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    var token= await this.api.get_token()
-    console.log("token on guard= ",token)
-    if (token!=undefined && token!=null) {
-      console.log("Utilisateur connecté")
-      await this.api.update_data_from_token()
-      return true
-    } else {
-      console.log("Utilisateur non connecté")
-      // this.routage.navigate(['/public/login'])
-      this.routage.navigate(['/public/login'], { queryParams: { returnUrl: state.url } });
-      return false
+  constructor(private api: ApiService, private routage: Router) { }
+
+  async canActivate(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    var infos = await this.api.get_from_local_storage("infos")
+    console.log("infos on guard= ", infos);
+    const token = await this.api.get_token();
+    if (token) {
+      await this.api.update_data_from_token();
+      if (!this.api.token.is_expired) {
+        await this.api.ensure_infos();   // crée/charge infos + reconstruit le menu
+        this.api.custom_menu();          // sécurité
+        return true;
+      }
     }
+    this.routage.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
   }
-  
 }
