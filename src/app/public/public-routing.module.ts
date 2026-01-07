6c885f6a2e4487b95525f75a08774dec3d9dca22
Abdoulaye Wélé <weleabdoulaye15@gmail.com>
2026-01-07 14:32:10 +0000

    Ajout fonctionnalité X

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListLoginComponent } from './login/list-login/list-login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: "", component: ListLoginComponent },
+  { path: "", component: LandingPageComponent },
+  { path: "Accueil", component: LandingPageComponent },
   { path: "login", component: ListLoginComponent }
 ];
 
